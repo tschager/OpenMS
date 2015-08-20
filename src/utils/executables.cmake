27b126c6e1c63bf64667e47093430b6af2550122
Hannes Roest <roest@imsb.biol.ethz.ch>
2015-08-19 23:02:53 -0700

    remove utils travis

--- src/utils/executables.cmake
+++ src/utils/executables.cmake
@@ -3,68 +3,10 @@ set(directory source/APPLICATIONS/UTILS)
 
 ### list all filenames of the directory here
 set(UTILS_executables
-AccurateMassSearch
-CVInspector
 DecoyDatabase
 DeMeanderize
-Digestor
-DigestorMotif
-ERPairFinder
-FeatureFinderSuperHirn
-FFEval
 FuzzyDiff
-IDDecoyProbability
-IDExtractor
-IDMassAccuracy
-IDScoreSwitcher
-IDSplitter
-LabeledEval
-LowMemPeakPickerHiRes
-LowMemPeakPickerHiRes_RandomAccess
-MapAlignmentEvaluation
-MassCalculator
-MetaboliteSpectralMatcher
-MetaProSIP
-MRMPairFinder
-MSSimulator
-OpenMSInfo
-PeakPickerIterative
-QCCalculator
-QCEmbedder
-QCExporter
-QCExtractor
-QCImporter
-QCMerger
-QCShrinker
-RNPxl
-RNPxlXICFilter
-RTAnnotator
-RTEvaluation
-SemanticValidator
-SequenceCoverageCalculator
-SimpleSearchEngine
-SpecLibCreator
-SvmTheoreticalSpectrumGeneratorTrainer
-TransformationEvaluation
-TopPerc
-XMLValidator
-#SimpleSearchEngine
-#RNPxlSearch
 )
-
-if(NOT DISABLE_OPENSWATH)
-  set(UTILS_executables
-    ${UTILS_executables}
-    ConvertTSVToTraML
-    ConvertTraMLToTSV
-    OpenSwathDIAPreScoring
-    OpenSwathMzMLFileCacher
-    OpenSwathWorkflow
-    OpenSwathRewriteToFeatureXML
-    MRMTransitionGroupPicker
-  )
-endif(NOT DISABLE_OPENSWATH)
-
 
 ## all targets requiring OpenMS_GUI
 set(UTILS_executables_with_GUIlib
